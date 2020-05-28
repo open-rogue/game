b2270569e184d641eb4845e8d42547e8c3ba52c6
Louis Machin <ldhmachin@gmail.com>
2020-05-28 05:25:42 +0100

    Added MMO source

---
+++ js/player.js
@@ -0,0 +1,153 @@
+class Player {
+  constructor(name, x, y, dir, is_player, is_moving, anim_frame) {
+  	this.name = name
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+    this.is_player = is_player;
+    this.is_moving = is_moving;
+    this.latestTime = Infinity;
+    this.anim_speed = 4;
+    this.anim_count = 0;
+    this.anim_frame = anim_frame;
+    if (this.is_player) {
+      this.submit();
+    }
+  }
+
+  move(x, y) {
+    var vector = createVector(x - this.x, y - this.y);
+    var mag = (vector.mag() > PLAYER_SPEED) ? PLAYER_SPEED : vector.mag();
+  	this.dir = vector.normalize();
+    var new_x = this.x + this.dir.x * mag;
+    var new_y = this.y + this.dir.y * mag;
+    if (this.checkPos(new_x, new_y)) {
+      this.x = new_x;
+      this.y = new_y;
+      this.anim_count++;
+      if (this.anim_count >= this.anim_speed) {
+        this.anim_count = 0;
+        this.anim_frame = (this.anim_frame == 0) ? 1 : 0;
+      }
+      this.submit();      
+    }
+  }
+
+  startMoving() {
+    this.is_moving = true;
+  }
+
+  stopMoving() {
+    this.is_moving = false;
+    this.submit();    
+  }
+
+  display() {
+    if (this.is_player) {
+      fill(255, 0, 0);
+    } else {
+      fill(128);
+    }
+    //ellipse(this.x, this.y, 16, 16);
+    if (!this.is_moving) {
+      image((this.dir.x < 0) ? player_left_img : player_right_img, this.x - TILESIZE/2, this.y - TILESIZE/2);
+    } else {
+      if (this.anim_frame == 0) {
+        image((this.dir.x < 0) ? player_left_walk_a_img : player_right_walk_a_img, this.x - TILESIZE/2, this.y - TILESIZE/2);
+      } else {
+        image((this.dir.x < 0) ? player_left_walk_b_img : player_right_walk_b_img, this.x - TILESIZE/2, this.y - TILESIZE/2);
+      }
+    }
+    line(this.x, this.y, this.x + this.dir.x * TILESIZE, this.y + this.dir.y * TILESIZE);
+  	text(this.name, this.x, this.y + 32);
+  }
+
+  submit() {
+  	var data = {
+  		x: this.x,
+  		y: this.y,
+  		dir: [this.dir.x, this.dir.y],
+      isMoving: this.is_moving,
+      animFrame: this.anim_frame,
+      lastAction: firebase.database.ServerValue.TIMESTAMP
+  	};
+  	var ref = database.ref('mmo/players');
+  	ref.child(this.name).set(data, this.gotData);
+  }
+
+  gotData(data) {
+
+  }
+
+  checkPos(new_x, new_y) {
+    // Check canvas edge collision and teleportation
+    // North
+    if (new_y - (TILESIZE / 2) < 0) {
+      if (tilemap.north > -1) {
+        print(tilemap.north);
+        tilemap = new TileMap(tilemap.north);
+        this.y = 32;
+      }
+      return false;
+    }
+    // East
+    if (new_x + (TILESIZE / 4) > width) {
+      if (tilemap.east > -1) {
+        print(tilemap.east);
+        tilemap = new TileMap(tilemap.east);
+        this.x = 32;
+      }
+      return false;
+    }
+    // South
+    if (new_y + (TILESIZE / 2) > height) {
+      if (tilemap.south > -1) {
+        print(tilemap.south);
+        tilemap = new TileMap(tilemap.south);
+        this.y = height - 32;
+      }
+      return false;
+    }
+    // West
+    if (new_x - (TILESIZE / 4) < 0) {
+      if (tilemap.west > -1) {
+        print(tilemap.west);
+        tilemap = new TileMap(tilemap.west);
+        this.x = width - 32;
+      }
+      return false;
+    }
+    //if (new_x - (TILESIZE / 4) < 0 || new_x + (TILESIZE / 4) > width) {
+    //  return false;
+    //}
+    //if (new_y - (TILESIZE / 2) < 0 || new_y + (TILESIZE / 2) > height) {
+    //  return false;
+    //}
+    // Check wall tile collision
+    for (var k = 0; k < tilemap.tiles.length; k++) {
+      var tile = tilemap.tiles[k];
+      if (tile.is_wall) {
+        if (createVector(new_x - tile.x(), new_y - tile.y()).mag() < 32) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  left() {
+    return this.x;
+  }
+
+  top() {
+    return this.y;
+  }
+
+  right() {
+    return this.x + TILESIZE;
+  }
+
+  bottom() {
+    return this.y + TILESIZE;
+  }
+}
---
+++ js/preload.js
@@ -0,0 +1,50 @@
+const TILESIZE = 32;
+const MAP_WIDTH = 16;
+const MAP_HEIGHT = 12;
+const PLAYER_SPEED = 8;
+
+let player_left_img;
+let player_right_img;
+
+let tileset = [];
+
+let canvas;
+let tilemap;
+let player;
+let ghosts = [];
+let database;
+let rooms;
+
+function preload() {
+	// Firebase
+	var firebaseConfig = {
+		apiKey: "AIzaSyAmxjDLZrtiWjQGkekCdTXKx5zCbLDJP28",
+		authDomain: "machin-dev.firebaseapp.com",
+		databaseURL: "https://machin-dev.firebaseio.com",
+		projectId: "machin-dev",
+		storageBucket: "machin-dev.appspot.com",
+		messagingSenderId: "471389656934",
+		appId: "1:471389656934:web:de807290048ad37fa91aa7",
+		measurementId: "G-2LDMEXJL3N"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
+	database = firebase.database();
+	// Player data event
+	var ref = database.ref('mmo/players');
+  	ref.on('value', gotPlayerData, errPlayerData);
+	// Room data event
+	var ref = database.ref('mmo/rooms');
+  	ref.on('value', gotRoomData, errRoomData);
+	// Tiles
+	player_left_img = loadImage('img/player_left.png');
+	player_right_img = loadImage('img/player_right.png');
+	player_left_walk_a_img = loadImage('img/player_left_walk_a.png');
+	player_right_walk_a_img = loadImage('img/player_right_walk_a.png');
+	player_left_walk_b_img = loadImage('img/player_left_walk_b.png');
+	player_right_walk_b_img = loadImage('img/player_right_walk_b.png');
+	tileset.push(loadImage('img/0.png'));
+	tileset.push(loadImage('img/1.png'));
+	tileset.push(loadImage('img/2.png'));
+}
