16335f54d431f0b265e9bbbbc00ced3d8c9e2633
Louis Machin <ldhmachin@gmail.com>
2020-06-05 09:25:36 +0100

    Added tile js

--- js/room.js
+++ js/room.js
@@ -1,24 +1,3 @@
-class Tile {
-  constructor(i, j, type) {
-  	this.i = i;
-    this.j = j;
-    this.type = type;
-    this.is_wall = (type != 0);
-  }
-
-  display() {
-    image(tileset[this.type], this.i * TILESIZE, this.j * TILESIZE);
-  }
-
-  x() {
-    return (this.i * TILESIZE) + (TILESIZE / 2);
-  }
-
-  y() {
-    return (this.j * TILESIZE) + (TILESIZE / 2);
-  }
-}
-
 class Room {
   constructor(room_id) {
     this.room_id = room_id;
