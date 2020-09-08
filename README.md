c25ae415f3f14c6afda2f7d8e4441b7ef518cd55
Louis Machin <ldhmachin@gmail.com>
2020-09-08 16:56:55 +0100

    Added title screen

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 [Mini Roguelike 8x8 Tiles - morgan3d](https://opengameart.org/content/mini-roguelike-8x8-tiles)
 
 [Pixel Fonts - pix3m](https://opengameart.org/content/pixel-fonts-by-pix3m)
+
+[16x16 Fantasy Tileset - Jerom](https://opengameart.org/content/16x16-fantasy-tileset)
---
+++ js/login.js
@@ -0,0 +1,11 @@
+function showGame() {
+    var game_div = document.getElementById("game");
+    var menu_div = document.getElementById("menu");
+    if (game_div.style.display == "none") {
+        game_div.style.display = "flex";
+        menu_div.style.display = "none";
+    } else {
+        game_div.style.display = "none";
+        menu_div.style.display = "flex";
+    }
+}
