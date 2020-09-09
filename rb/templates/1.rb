e43de5eb67489094b59c02558c2bb63b8531f9e9
Louis Machin <ldhmachin@gmail.com>
2020-09-09 12:02:21 +0100

    Updated rooms

--- rb/templates/1.rb
+++ rb/templates/1.rb
@@ -8,33 +8,31 @@ room.random_fill('*', 0.1)
 
        #012345678901234567890123
 map  = "OOOOOOOOOOOOOOOOOOOOOOOO" #0
-map += "O                      O" #1
-map += "O                      O" #2
-map += "O    ┌────┌─────┐      O" #3
-map += "O    <GGG^<GGGGGG      O" #4
-map += "O    <GGGGGGGGGGG      O" #5
-map += "O    <GGGGGGGGGGG      O" #6
-map += "O    <GGGGGGGGGG>      O" #7
-map += "O    <GGGGGGGGGG>      O" #8
-map += "O    └────└─────┘      O" #9
-map += "O                      O" #0
-map += "O             S        O" #1
-map += "O                      O" #2
-map += "O                      O" #3
-map += "O                      O" #4
+map += "OOOOOO            OOOOOO" #1
+map += "OOO                 OOOO" #2
+map += "OO    ┌────┌─────┐    OO" #3
+map += "O     <GGG^<GGGGGG     O" #4
+map += "O     <GGGGGGGGGGG     O" #5
+map += "O     <GGGGGGGGGGG     O" #6
+map += "O     <GGGGGGGGGG>     O" #7
+map += "O     <GGGGGGGGGG>     O" #8
+map += "O     └────└─────┘     O" #9
+map += "OO                     O" #0
+map += "OO            S        O" #1
+map += "OOO                   OO" #2
+map += "OOOOOO               OOO" #3
+map += "OOOOOOOO          OOOOOO" #4
 map += "OOOOOOOOOO    OOOOOOOOOO" #5
 
 room.overlay(map)
-room.tile(16, 4, "WALL_NSE_N")
-room.tile(16, 6, "WALL_NSE_S")
-room.tile(10, 5, "WALL_NSW_N")
-room.tile(10, 8, "WALL_NSW_S")
-room.warp(9, 4, 3, 9, 6)
+room.tile(17, 4, "WALL_NSE_N")
+room.tile(17, 6, "WALL_NSE_S")
+room.tile(11, 5, "WALL_NSW_N")
+room.tile(11, 8, "WALL_NSW_S")
+room.warp(10, 4, 3, 9, 6)
 room.sign(14, 11, "This is a test sign")
 
-
 room.reformat()
-room.draw()
 
 room.save()
 
