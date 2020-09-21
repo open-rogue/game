b0dac4ffacbbda0f5c599ae16406cef8fe0f3b2d
Louis Machin <ldhmachin@gmail.com>
2020-09-21 10:00:42 +0100

    Added chat commands

--- js/chat.js
+++ js/chat.js
@@ -5,3 +5,15 @@ function chatValue() {
 function clearChat() {
   document.getElementById("chat").value = "";
 }
+
+function chatExecute(str) {
+  if (str[0] != "/") { return null }
+  command = str.substring(1);
+  if (command == "spawn") {
+    var x = 0.5 * TILESIZE * MAP_WIDTH;
+    var y = 0.5 * TILESIZE * MAP_HEIGHT;
+    print(x, y);
+    player.changeRoom(START_ROOM, x, y);
+    return true;
+  }
+}
