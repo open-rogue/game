7e2a6eb60f57f9f44574a42e50681a8b1b43d520
Louis Machin <ldhmachin@gmail.com>
2020-09-21 01:34:34 +0100

    Added realtime chat

--- js/stats.js
+++ js/stats.js
@@ -5,9 +5,15 @@ class Stats {
 
     display() {
         fill("#FFFFFF");
-        image(tileset["GOLD"], 0, height - TILESIZE, TILESIZE, TILESIZE);
         textAlign(LEFT, CENTER);
         textSize(16);
-        text(" * " + player.inventory.GOLD, TILESIZE * 1, height - TILESIZE - 1, TILESIZE * 4, TILESIZE);
+        // Gold
+        var quantity = player.inventory.GOLD == null ? 0 : player.inventory.GOLD;
+        image(tileset["GOLD"], 0, height - TILESIZE, TILESIZE, TILESIZE);
+        text(" * " + quantity, TILESIZE * 1, height - TILESIZE - 1, TILESIZE * 4, TILESIZE);
+        // Potion
+        var quantity = player.inventory.POTION == null ? 0 : player.inventory.POTION;
+        image(tileset["POTION"], TILESIZE * 4, height - TILESIZE, TILESIZE, TILESIZE);
+        text(" * " + quantity, TILESIZE * 5, height - TILESIZE - 1, TILESIZE * 4, TILESIZE);
     }
 }
