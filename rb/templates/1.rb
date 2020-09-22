fb7642ca47adffe08f4f0279dbc55adb13727acc
Louis Machin <ldhmachin@gmail.com>
2020-09-22 16:21:12 +0100

    Updated wall tiles

--- rb/templates/1.rb
+++ rb/templates/1.rb
@@ -4,19 +4,20 @@ room = Room.new(1)
 room.south = 2
 
 room.fill(" ")
+room.random_fill('"', 0.1)
 room.random_fill('*', 0.1)
 
        #012345678901234567890123
 map  = "OOOOOOOOOOOOOOOOOOOOOOOO" #0
 map += "OOOOOO            OOOOOO" #1
 map += "OOO                 OOOO" #2
-map += "OO    ┌────┌─────┐    OO" #3
-map += "O     <GGG^<GGGGGG     O" #4
-map += "O     <GGGGGGGGGGG     O" #5
-map += "O     <GGGGGGGGGGG     O" #6
-map += "O     <GGGGGGGGGG>     O" #7
-map += "O     <GGGGGGGGGG>     O" #8
-map += "O     └────└─────┘     O" #9
+map += "OO    ┌──┄─┬─┄┄─┄┐    OO" #3
+map += "O     |GGG^|GGGGG╵     O" #4
+map += "O     |GGGG╵GGGGGG     O" #5
+map += "O     |GGGGGGGGGG╷     O" #6
+map += "O     |GGGG╷GGGGG|     O" #7
+map += "O     |GGGG|GGGGG|     O" #8
+map += "O     └┄───┴┄──┄─┘     O" #9
 map += "OO                     O" #0
 map += "OO            S        O" #1
 map += "OOO                   OO" #2
@@ -25,10 +26,6 @@ map += "OOOOOOOO          OOOOOO" #4
 map += "OOOOOOOOOO    OOOOOOOOOO" #5
 
 room.overlay(map)
-room.tile(17, 4, "WALL_NSE_N")
-room.tile(17, 6, "WALL_NSE_S")
-room.tile(11, 5, "WALL_NSW_N")
-room.tile(11, 8, "WALL_NSW_S")
 room.warp(10, 4, 3, 9, 6)
 room.sign(14, 11, "This is a test sign")
 
--- rb/templates/3.rb
+++ rb/templates/3.rb
@@ -9,13 +9,13 @@ room.fill(" ")
 map  = "                        " #0
 map += "                        " #1
 map += "                        " #2
-map += "      ┌────┌─────┐      " #3
-map += "      <GGG˅<G   G>      " #4
-map += "      <GGGGGGGGGG>      " #5
-map += "      <GGGGGGGGGG>      " #6
-map += "      <GGGGGGGGGG>      " #7
-map += "      <GGGGGGGGGG>      " #8
-map += "      └──────────┘      " #9
+map += "      ┌────┬─────┐      " #3
+map += "      |   ˅|     |      " #4
+map += "      |    ╵     |      " #5
+map += "      |          |      " #6
+map += "      |    ╷     |      " #7
+map += "      |    |     |      " #8
+map += "      └────┴─────┘      " #9
 map += "                        " #0
 map += "                        " #1
 map += "                        " #2
@@ -27,7 +27,6 @@ room.overlay(map)
 room.tile(13, 4, "CHAIR")
 room.tile(14, 4, "TABLE")
 room.tile(15, 4, "CHAIR")
-room.tile(11, 5, "WALL_NSW_N")
 room.warp(10, 4, 1, 9, 6)
 
 room.reformat()
