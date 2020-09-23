c07a116c4eca9d03313d37f31fabe468e591ecd7
Louis Machin <ldhmachin@gmail.com>
2020-09-23 15:37:31 +0100

    Added skin command

--- js/chat.js
+++ js/chat.js
@@ -8,17 +8,22 @@ function clearChat() {
 
 function chatExecute(str) {
 	if (str[0] != "/") { return null }
-	command = str.substring(1);
-	if (command == "spawn") {
+	command = str.substring(1).split(" ");
+	if (command[0] == "spawn") {
 		player.changeRoom(START_ROOM, 0.5 * mapWidth(), 0.5 * mapHeight());
 		return true;
 	}
-	if (command == "rain") {
+	if (command[0] == "rain") {
 		weather.toggle(WEATHER_RAIN);
 		return true;
 	}
-	if (command == "snow") {
+	if (command[0] == "snow") {
 		weather.toggle(WEATHER_SNOW);
+		return true;
+	}
+	if (command[0] == "skin") {
+		print(command[1]);
+		player.changePlayerType(command[1]);
 		return true;
 	}
 }
