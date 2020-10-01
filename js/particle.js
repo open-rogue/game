445ee06f956135c6ce840d1a00f7964d629d32e0
Louis Machin <ldhmachin@gmail.com>
2020-10-01 15:22:01 +0100

    Refactor

--- js/particle.js
+++ js/particle.js
@@ -1,18 +1,24 @@
 class Particle { 
-    constructor(type, x, y, t, size = 16) {
+    constructor(type, x, y, t, fade = true, rise = true, size = 16) {
         this.type = (type in tileset) ? type : "NULL";
         this.x = x;
         this.y = y;
-        this.t = t;
+		this.t = t; // Decremented value
+		this.o = t; // Original value
+		this.fade = fade;
+		this.rise = rise;
         this.size = size;
     }
     
     display() {
+		if (this.fade) { tint(255, 255 * this.t / this.o) }
     	image(tileset[this.type], this.x - this.size/2, this.y - this.size/2, this.size, this.size);
-        this.decrease();
+		noTint();
+		this.decrease();
     }
 
     decrease() {
+		if (this.rise) { this.y -= 2 }
         return this.t -= 1;
     }
 
