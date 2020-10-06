5bb9c7cb8f91f0dfdb7f8853858cdee0622fa090
mejszin <louis@formka.com>
2020-10-06 16:59:01 +0100

    Adding layout page

--- css/register.js
+++
@@ -1,8 +0,0 @@
-function registerAccount() {
-    div = document.getElementById("form");
-    div.innerHTML = `
-        <h1>Create new player</h1>
-        <input>Name</input>
-        <button>Okay!</button>
-    `;
-}
